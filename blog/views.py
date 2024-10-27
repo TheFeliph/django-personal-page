c8585e3070581c99ec4557e5b8f976dfe18a1555
Felipe Nasciemnto <Feliph08@hotmail.com>
2024-10-27 04:15:17 -0300

    Adicionar base.html e index.html para estrutura de templates no blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_view(request):
+    return render(request, 'blog/templates/blog/index.html')  # Use o caminho correto para o template
