ac4a1b203871116f250c20f6e77fce2ef03eb336
Felipe Nasciemnto <Feliph08@hotmail.com>
2024-10-29 01:19:47 -0300

    Adiciona views de home e post

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
 URL configuration for personal_page project.
 
@@ -14,10 +15,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+<<<<<<<< HEAD:blog/urls.py
 from django.urls import path
 from .views import post_view
 
 urlpatterns = [
     path('post/', post_view, name='post'),
+========
+# personal_page/urls.py
+
+from django.contrib import admin
+from django.urls import path, include  # Importando include para usar as URLs do app
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),  # Inclui as URLs do aplicativo blog
+>>>>>>>> 25e1bf3 (Adiciona views de home e post):personal_page/urls.py
 ]
 
+=======
+# blog/urls.py
+
+from django.urls import path
+from .views import post_view, home_view  # Certifique-se de que home_view e post_view estão importados
+
+urlpatterns = [
+    path('', home_view, name='home'),       # URL raiz do blog (opcional)
+    path('post/', post_view, name='post_view'),  # URL para a view de Post
+]
+>>>>>>> 25e1bf3 (Adiciona views de home e post)
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
+<<<<<<< HEAD
+=======
+def home_view(request):
+    return HttpResponse("Welcome to the Home Page!")
+
+>>>>>>> 25e1bf3 (Adiciona views de home e post)
 def post_view(request):
     return HttpResponse("Hello World")
