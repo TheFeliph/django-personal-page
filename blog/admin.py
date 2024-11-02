89c13a897ee4edfe368022659c744bd8b0cad5eb
Felipe Nasciemnto <Feliph08@hotmail.com>
2024-11-02 20:54:13 -0300

    Atualiza modelo Post e configurações de admin para exibir todos os campos necessários

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created', 'updated')
+    list_filter = ('status', 'created')
+    ordering = ('status', 'created')
+    date_hierarchy = 'created'
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    slug = models.SlugField(unique=True)  # Campo para o slug, deve ser único
+    created = models.DateTimeField(auto_now_add=True)  # Campo de data/hora de criação
+    updated = models.DateTimeField(auto_now=True)  # Campo de data/hora de atualização
+    status = models.CharField(max_length=10, choices=[('draft', 'Draft'), ('published', 'Published')], default='draft')  # Campo de status
 
     def __str__(self):
         return self.title
